b87cb6143d1051d25ee2c5293baa490975f447c4
garrettmend <u1305528@umail.utah.edu>
2023-02-20 22:58:59 -0700

    final

--- app/src/main/java/com/example/examples/DisplayActivity.kt
+++ app/src/main/java/com/example/examples/DisplayActivity.kt
@@ -1,4 +1,7 @@
+package com.example.examples
+
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.examples.R
@@ -8,6 +11,9 @@ class DisplayActivity : AppCompatActivity() {
     private var mFName: String? = null
     private var mMName: String? = null
     private var mLName: String? = null
+    private var mFNameText: TextView? = null
+    private var mMNameText: TextView? = null
+    private var mLNameText: TextView? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +26,34 @@ class DisplayActivity : AppCompatActivity() {
         mMName = receivedIntent.getStringExtra("ET_MN")
         mLName = receivedIntent.getStringExtra("ET_LN")
 
-        Toast.makeText(
-            this@DisplayActivity,
-            "Enter only first and last name!",
-            Toast.LENGTH_SHORT
-        ).show()
+        mFNameText = findViewById(R.id.fn)
+        mMNameText = findViewById(R.id.mn)
+        mLNameText = findViewById(R.id.ln)
+
+        mFNameText!!.text = mFName
+        mMNameText!!.text = mMName
+        mLNameText!!.text = mLName
+
+
+    }
+    override fun onSaveInstanceState(out: Bundle) {
+
+        super.onSaveInstanceState(out)
+
+        mFName = mFNameText!!.text.toString()
+        mMName = mMNameText!!.text.toString()
+        mLName = mLNameText!!.text.toString()
+
+        out.putString("ET_FN", mFName)
+        out.putString("ET_MN", mMName)
+        out.putString("ET_LN", mLName)
+    }
+    override fun onRestoreInstanceState(saved: Bundle) {
+
+        super.onRestoreInstanceState(saved)
+
+        mFNameText!!.text = saved.getString("ET_FN")
+        mMNameText!!.text = saved.getString("ET_MN")
+        mLNameText!!.text = saved.getString("ET_LN")
     }
 }
--- app/src/main/java/com/example/examples/MainActivity.kt
+++ app/src/main/java/com/example/examples/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.examples
 
-import DisplayActivity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
@@ -9,21 +8,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.Button
 
 
     class MainActivity : AppCompatActivity(), View.OnClickListener {
         private var mFName: String? = null
         private var mMName: String? = null
         private var mLName: String? = null
-        private var fullName: String? = null
 
-        private var mTvFirstName: TextView? = null
-        private var mTvLastName: TextView? = null
+        private var mFirstNameText: TextView? = null
+        private var mMiddleNameText: TextView? = null
+        private var mLastNameText: TextView? = null
 
         private var mButtonSubmitFn: Button? = null
         private var mButtonSubmitMn: Button? = null
@@ -36,8 +35,8 @@ import androidx.activity.result.contract.ActivityResultContracts
 
         private var mPic: ImageView? = null
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+        override fun onCreate(saved: Bundle?) {
+            super.onCreate(saved)
             setContentView(R.layout.activity_main)
 
             //button
@@ -51,10 +50,25 @@ import androidx.activity.result.contract.ActivityResultContracts
             mButtonSubmitFn!!.setOnClickListener(this)
             mButtonSubmitMn!!.setOnClickListener(this)
             mButtonSubmitLn!!.setOnClickListener(this)
+            mButtonCam!!.setOnClickListener(this)
+            mButtonSubmitFinal!!.setOnClickListener(this)
         }
 
-        override fun onClick(v: View?) {
-            when (v?.id) {
+        override fun onClick(view: View?) {
+            when (view?.id) {
+                R.id.button_submit_fn ->{
+                    mEtFName = findViewById(R.id.et_name_fn)
+                    mFName = mEtFName!!.text.toString()
+
+                }
+                R.id.button_submit_mn -> {
+                    mEtMName = findViewById(R.id.et_name_mn)
+                    mMName = mEtMName!!.text.toString()
+                }
+                R.id.button_submit_ln -> {
+                    mEtLName = findViewById(R.id.et_name_ln)
+                    mLName = mEtLName!!.text.toString()
+                }
                 R.id.button_submit_final -> {
                     mEtFName = findViewById(R.id.et_name_fn)
                     mEtMName = findViewById(R.id.et_name_mn)
@@ -63,6 +77,7 @@ import androidx.activity.result.contract.ActivityResultContracts
                     mFName = mEtFName!!.text.toString()
                     mMName = mEtMName!!.text.toString()
                     mLName = mEtLName!!.text.toString()
+
 
                     //goes to displayactivity
                     val messageIntent = Intent(this, DisplayActivity::class.java)
@@ -73,18 +88,53 @@ import androidx.activity.result.contract.ActivityResultContracts
                 }
                 R.id.button_pic -> {
                     val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    try{
+                        cameraActivity.launch(cameraIntent)
+                    }catch(ex:ActivityNotFoundException){
+                        Toast.makeText(
+                            this@MainActivity,
+                            "CAMERA FAIL!",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
                 }
 
 
             }
+        }
+        override fun onSaveInstanceState(out: Bundle) {
+
+            super.onSaveInstanceState(out)
+
+            try {
+                mFName = mEtFName!!.text.toString()
+                mMName = mEtMName!!.text.toString()
+                mLName = mEtLName!!.text.toString()
+
+                out.putString("ET_FN", mFName)
+                out.putString("ET_MN", mMName)
+                out.putString("ET_LN", mLName)
+            }catch(ex:NullPointerException){
+                Toast.makeText(
+                    this@MainActivity,
+                    "Empty Fields!!!!!!!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        override fun onRestoreInstanceState(saved: Bundle) {
+
+            super.onRestoreInstanceState(saved)
+
+            mFirstNameText!!.text = saved.getString("ET_FN")
+            mMiddleNameText!!.text = saved.getString("ET_MN")
+            mLastNameText!!.text = saved.getString("ET_LN")
         }
 
         private val cameraActivity =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == RESULT_OK) {
                     mPic = findViewById<View>(R.id.iv_pic) as ImageView
-                    //val extras = result.data!!.extras
-                    //val thumbnailImage = extras!!["data"] as Bitmap?
 
                     if (Build.VERSION.SDK_INT >= 33) {
                         val thumbnailImage =
